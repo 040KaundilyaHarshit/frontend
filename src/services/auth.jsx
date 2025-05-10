c27fff147ea6a4504dc20fb9b6c89581abb9e2a3
040KaundilyaHarshit <harshitanandbuban123@gmail.com>
2025-05-10 19:31:01 +0530

    fix : backend endpoint

--- src/services/auth.jsx
+++ src/services/auth.jsx
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:3001"; // Backend URL
 
 // Register a new user
 export const registerUser = async (email, password, role) => {
   try {
-    const response = await axios.post(`${API_URL}/register`, { email, password, role });
+    const response = await axios.post(`${VITE_BACKEND_URL}/register`, { email, password, role });
     return response.data;
   } catch (error) {
     return error.response.data;
@@ -15,7 +14,7 @@ export const registerUser = async (email, password, role) => {
 // Login user
 export const loginUser = async (email, password) => {
   try {
-    const response = await axios.post(`${API_URL}/login`, { email, password });
+    const response = await axios.post(`${VITE_BACKEND_URL}/login`, { email, password });
     return response.data;
   } catch (error) {
     return error.response.data;
@@ -26,7 +25,7 @@ export const loginUser = async (email, password) => {
 export const getCourses = async () => {
   const token = localStorage.getItem("token");
   try {
-    const response = await axios.get(`${API_URL}/api/courses`, {
+    const response = await axios.get(`${VITE_BACKEND_URL}/api/courses`, {
       headers: { "x-auth-token": token },
     });
     return response.data;
