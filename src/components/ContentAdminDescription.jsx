085d4ce6737d25204ae0f1c17a992f79f39564d6
040KaundilyaHarshit <harshitanandbuban123@gmail.com>
2025-05-10 16:25:54 +0530

    changed Case Sensetive Error

--- src/components/ContentAdminDescription.jsx
+++ src/components/ContentAdminDescription.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
-import "./contentAdminDescription.css";
+import "./ContentAdminDescription.css";
 
 const ContentAdminDescription = () => {
   const { courseId } = useParams();
